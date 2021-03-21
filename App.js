66b1e25801a8a5b7087c522ed982aa936cb1e0f3
adamcasey <adam-casey@live.com>
2021-03-21 06:18:33 -0600

    Start logic for new matchday

--- App.js
+++ App.js
@@ -2,11 +2,16 @@
 const fs = require('fs');
 const readline = require('readline');
 const soccerHelpers = require('./SoccerHelper');
+// TODO: Only import lodash methods you use
+const _ = require('lodash');
 
 class SoccerMatches {
 	constructor(file) {
 		this.inputFile = file;
 		this.teamArray = [];
+		this.seenTeams = {};
+		this.matchDayNum = 0;
+		this.seenTeamsFlag = false;
 	}
 
 	get fileToRead() {
@@ -17,36 +22,8 @@ class SoccerMatches {
 		return /\d+/;
 	}
 
-	getScore = (line) => {
-		return parseInt(line);
-	};
-
-	getTeams = (line) => {
-		return line.split(',');
-	};
-
-	getTeamName = (team) => {
-		return team.replace(/\d+/g, '').trim();
-	};
-
-	getWinnerAndScore = (teamArray) => {
-		const firstScore = teamArray[0].match(this.regDigit);
-		const secondScore = teamArray[1].match(this.regDigit);
-		let teamName = '';
-		if (firstScore > secondScore) {
-			teamName = this.getTeamName(teamArray[0]);
-			return [teamName, firstScore[0]];
-		}
-		if (secondScore > firstScore) {
-			teamName = this.getTeamName(teamArray[1]);
-			return [teamName, secondScore[0]];
-		}
-		if (firstScore === secondScore) return;
-	};
-
-	readFile = async () => {
+	getMatchDay = async () => {
 		try {
-			// console.log('soccerHelp: ', soccerHelpers.add(4, 4));
 			const readInterface = readline.createInterface({
 				input: fs.createReadStream(this.fileToRead),
 				crlfDelay: Infinity,
@@ -56,29 +33,53 @@ class SoccerMatches {
 			for await (const line of readInterface) {
 				try {
 					if (line.length) {
-						const teamArray = this.getTeams(line);
-						const winnerAndScore = this.getWinnerAndScore(teamArray);
-						if (winnerAndScore) {
-							console.log('winnder is: ', winnerAndScore);
+						const teamArray = soccerHelpers.getTeams(line);
+						const winnerAndScore = soccerHelpers.getWinnerAndScore(
+							teamArray,
+							this.regDigit
+						);
+						if (winnerAndScore.length === 1) {
+							checkSeenTeam(winnerAndScore[0], this.seenTeams)
+								? // ? (this.seenTeams[winnerAndScore[0]] += 3)
+								  handleSeenTeam(
+										winnerAndScore[0],
+										this.seenTeams,
+										3,
+										this.seenTeamsFlag,
+										this.teamArray
+								  )
+								: (this.seenTeams[winnerAndScore[0]] = 3);
+						} else if (winnerAndScore.length === 2) {
+							checkSeenTeam(winnerAndScore[0], this.seenTeams)
+								? // ? (this.seenTeams[winnerAndScore[0]] += 1)
+								  handleSeenTeam(
+										winnerAndScore[0],
+										this.seenTeams,
+										1,
+										this.seenTeamsFlag,
+										this.teamArray
+								  )
+								: (this.seenTeams[winnerAndScore[0]] = 1);
+							checkSeenTeam(winnerAndScore[1], this.seenTeams)
+								? // ? (this.seenTeams[winnerAndScore[1]] += 1)
+								  handleSeenTeam(
+										winnerAndScore[1],
+										this.seenTeams,
+										1,
+										this.seenTeamsFlag,
+										this.teamArray
+								  )
+								: (this.seenTeams[winnerAndScore[1]] = 1);
 						} else {
-							console.log('tie between: ', teamArray[0], ' and ', teamArray[1]);
+							console.log('Could not determine winner');
 						}
-
-						// console.log('first team: ', teamArray[0]);
-						// if (firstScore && secondScore) {
-						// 	console.log('firstScore: ', firstScore[0]);
-						// 	console.log('secondScore: ', secondScore[0]);
-						// 	this.teamArray.push(teamArray);
-						// }
 					}
 				} catch (error) {
 					console.log('Error getting winner: ', error);
 				}
 			}
-			// }
 
-			// return matchArray;
-			return this.teamArray;
+			return;
 		} catch (err) {
 			console.log('readfile error: ', error);
 		}
@@ -87,7 +88,12 @@ class SoccerMatches {
 
 const soccerMatch = new SoccerMatches(process.argv[2]);
 // console.log(soccerMatch.readFile());
-soccerMatch.readFile().then((res) => {
+soccerMatch.getMatchDay().then((res) => {
+	const sortedTeams = _.fromPairs(
+		_.sortBy(_.toPairs(soccerMatch.seenTeams), 1).reverse()
+	);
+	console.log('seenTeams: ', sortedTeams);
+	console.log('teamArray: ', soccerMatch.teamArray);
 	// console.log('res: ', res);
 	// console.log('soccerMatch: ', soccerMatch.teamArray);
 });
--- SoccerHelper.js
+++ SoccerHelper.js
@@ -1,11 +1,55 @@
-const add = (x, y) => {
-	return x + y;
+getScore = (line) => {
+	return parseInt(line);
 };
 
-const subtract = (x, y) => {
-	return x - y;
+getTeams = (line) => {
+	return line.split(',');
 };
 
-const num = 33;
+getTeamName = (team) => {
+	return team.replace(/\d+/g, '').trim();
+};
 
-module.exports = { add, subtract, num };
+getWinnerAndScore = (teamArray, regDigit) => {
+	const firstScoreArray = teamArray[0].match(regDigit);
+	const secondScoreArray = teamArray[1].match(regDigit);
+	const firstScoreNum = parseInt(firstScoreArray[0]);
+	const secondScoreNum = parseInt(secondScoreArray[0]);
+	let teamName = '';
+	if (firstScoreNum > secondScoreNum) {
+		teamName = getTeamName(teamArray[0]);
+		return [teamName];
+	}
+	if (secondScoreNum > firstScoreNum) {
+		teamName = getTeamName(teamArray[1]);
+		return [teamName];
+	}
+	if (firstScoreNum === secondScoreNum) {
+		const firstTeamName = getTeamName(teamArray[0]);
+		const secondTeamName = getTeamName(teamArray[1]);
+
+		return [firstTeamName, secondTeamName];
+	}
+};
+
+checkSeenTeam = (teamName, seenTeams) => {
+	return seenTeams[teamName];
+};
+
+handleSeenTeam = (teamName, seenTeams, scoreToAdd, seenFlag, teamArray) => {
+	teamArray.push(seenTeams);
+	seenTeams = {};
+	seenTeams[teamName] += scoreToAdd;
+	seenFlag = true;
+	return seenTeams;
+};
+
+// module.exports = { add, subtract, num, getScore, getTeams, getTeamName };
+module.exports = {
+	getScore,
+	getTeams,
+	getTeamName,
+	getWinnerAndScore,
+	checkSeenTeam,
+	handleSeenTeam,
+};
